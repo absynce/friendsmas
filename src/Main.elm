3a497a9fc098b9f9d8d66fae6aa8698a919ec976
Jared M. Smith <absynce@gmail.com>
2018-12-28 15:38:39 -0600

    Add opening paragraphs and Suspekt, Dang Heathens music

--- src/Main.elm
+++ src/Main.elm
@@ -1,12 +1,13 @@
 module Main exposing (main)
 
 import Browser
-import Element exposing (Element, alignRight, centerX, el, fill, padding, rgb255, row, spacing, text, width)
+import Element exposing (..)
 import Element.Background as Background
 import Element.Border as Border
 import Element.Font as Font
 import Element.Region as Region
 import Html exposing (Html)
+import Html.Attributes exposing (attribute)
 import Http
 import Json.Decode as Decode exposing (field)
 
@@ -98,11 +99,17 @@ viewPage model =
 
 
 viewPageSuccess model =
-    Element.layout [ Element.explain Debug.todo ] <|
-        Element.row
-            [ width fill
+    Element.layout
+        [ Element.explain Debug.todo
+        , width fill
+        ]
+    <|
+        Element.column
+            [ width (fill |> maximum 1024)
+            , centerX
             ]
             [ viewHeading model
+            , viewContent model
             ]
 
 
@@ -115,9 +122,63 @@ viewHeading model =
         (text "Friendsmas")
 
 
+viewContent model =
+    Element.column
+        [ Element.explain Debug.todo
+        ]
+        [ Element.paragraph []
+            [ text "Thanks, for being a friend!"
+            ]
+        , Element.paragraph []
+            [ text "In addition to the physical thrift finds, you are treated to the following Evansville area music found online:"
+            ]
+        , viewMusic model
+        ]
+
+
+viewMusic model =
+    Element.column
+        [ Element.explain Debug.todo
+        , width fill
+        ]
+        [ el [ Region.heading 3 ] <|
+            text "Dang Heathens"
+        , dangHeathens
+        , el [ Region.heading 3 ] <|
+            text "Suspekt"
+        , cicada
+        ]
+
+
 viewError : String -> Html Msg
 viewError errorMessage =
     Html.text <| "Error: " ++ errorMessage
+
+
+cicada =
+    html <|
+        Html.iframe
+            [ attribute "allow" "autoplay"
+            , attribute "frameborder" "no"
+            , attribute "height" "166"
+            , attribute "scrolling" "no"
+            , Html.Attributes.src "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/145253471%3Fsecret_token%3Ds-Ek7mG&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+            , attribute "width" "100%"
+            ]
+            []
+
+
+dangHeathens =
+    html <|
+        Html.iframe
+            [ attribute "frameborder" "no"
+            , attribute "height" "265"
+            , attribute "scrolling" "no"
+            , Html.Attributes.src "https://www.reverbnation.com/widget_code/html_widget/artist_2445122?widget_id=55&pwc[included_songs]=1&context_type=page_object&pwc[size]=small"
+            , attribute "style" "width:0px;min-width:100%;max-width:100%;"
+            , attribute "width" "100%"
+            ]
+            []
 
 
 
